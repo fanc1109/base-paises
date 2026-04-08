0591d1dcf43682ee881e1588568bfb2f52e1a961
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-04-08 16:51:33 -0300

    j

--- bases/src/app/pages/sobre/sobre.ts
+++ bases/src/app/pages/sobre/sobre.ts
@@ -1,15 +1,9 @@
-
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sobre',
-<<<<<<< HEAD
-  standalone: true,
-=======
->>>>>>> f2cd4cf0bf95bb0637ec7c61d4530e7751caf390
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './sobre.html',
-  styleUrl: './sobre.css'
+  styleUrl: './sobre.css',
 })
+export class Sobre {}
