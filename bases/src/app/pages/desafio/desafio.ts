2a2e335cf4a8167bbf072a8a5ff055c56c6b66c5
amandarosadonascimento06-ai <amandarosadonascimento06@gmail.com>
2026-04-08 14:36:54 -0300

    aa

--- bases/src/app/pages/desafio/desafio.ts
+++ bases/src/app/pages/desafio/desafio.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-desafio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './desafio.html',
   styleUrl: './desafio.css',
 })
