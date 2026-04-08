af310539878c29c2bc3af9a2e61b17abbced30a4
amandarosadonascimento06-ai <amandarosadonascimento06@gmail.com>
2026-04-08 14:24:49 -0300

    aa

---
+++ bases/src/app/pages/desafio/desafio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desafio',
+  imports: [],
+  templateUrl: './desafio.html',
+  styleUrl: './desafio.css',
+})
+export class Desafio {}
