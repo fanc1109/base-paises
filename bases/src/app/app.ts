39f9b3f5626bd6c32b2c8b7cb23fa95546e22858
Francy L. Schenkel <francylauana11@gmail.com>
2026-04-08 16:33:14 -0300

    .

--- bases/src/app/app.ts
+++ bases/src/app/app.ts
@@ -5,12 +5,11 @@ import { Situacao } from './pages/situacao/situacao';
 import { Footer } from './layout/footer/footer';
 import { Header } from './layout/header/header';
 import { Home } from './pages/home/home';
-import { routes } from './app.routes';
 import { Sobre } from './pages/sobre/sobre';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,Home,Header,Footer,Desafio,Situacao,Sobre,routes],
+  imports: [RouterOutlet,RouterLink,Home,Header,Footer,Desafio,Situacao,Sobre],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
