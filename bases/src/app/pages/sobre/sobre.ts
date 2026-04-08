f2cd4cf0bf95bb0637ec7c61d4530e7751caf390
Francy L. Schenkel <francylauana11@gmail.com>
2026-04-08 16:35:30 -0300

    .

--- bases/src/app/pages/sobre/sobre.ts
+++ bases/src/app/pages/sobre/sobre.ts
@@ -1,8 +1,10 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sobre.html',
   styleUrl: './sobre.css',
 })
